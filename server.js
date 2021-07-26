e8764ba9e14d9babea565baf2e5aa195f8fcef4a
Fábia Emanuele Cardoso Sena <61851602+fabiacardoso@users.noreply.github.com>
2021-07-26 15:29:27 -0300

    Delete server.js

--- server.js
+++
@@ -1,6 +0,0 @@
-const  app  =  require ( "./src/app" )
- porta  const =  8080
-
-app . escuta ( processo . env . PORT  ||  porta ,  ( )  =>  {
-    console . log ( `App rodando na porta $ { port } ` )
-} )
