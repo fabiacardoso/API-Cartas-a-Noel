5172ce9bc765db15c1357c7d5fe8ebe97b62bd84
Fábia Emanuele Cardoso Sena <61851602+fabiacardoso@users.noreply.github.com>
2021-07-26 15:29:04 -0300

    Delete src directory

--- src/routes/authRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const controller = require("../controllers/authController")
-
-router.post('/signup', controller.signup)
-router.post('/login', controller.login)
-
-
-module.exports = router
